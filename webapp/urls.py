6f78612e8fda37f8d0aaea5261050deb5030c3c3
sgrozny <stanislavgrozny@gmail.com>
2017-01-26 12:52:18 -0500

    VS files moved over to match the proper structure

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,5 +2,29 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    #url(r'^$', views.index, name='index')
+url(r'^$', app.views.home, name='home'),
+    url(r'^createEvent$', app.views.createEvent, name='createEvent'),
+    url(r'^admin', app.views.admin, name='admin'),
+	url(r'^publish', app.views.publish, name='publish'),
+	url(r'^pubStatus', app.views.pubStatus, name='pubStatus'),
+    url(r'^login/$',
+        django.contrib.auth.views.login,
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': app.forms.BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title': 'Log in',
+                'year': datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        django.contrib.auth.views.logout,
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
     ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,76 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'webapp/main.html')
+#def index(request):
+#    return render(request, 'webapp/main.html')
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Syndication Tool',
+            'year':datetime.now().year,
+        }
+    )
+
+def createEvent(request):
+    """Renders the createEvent page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/createEvent.html',
+        {
+            'title':'Create Event',
+            'message':'Your Event Creation page.',
+            'year':datetime.now().year,
+        }
+    )
+def publish(request):
+    """Renders the createEvent page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/publish.html',
+        {
+            'title':'Publish event',
+            'message':'Your Event Creation page.',
+            'year':datetime.now().year,
+        }
+    )
+def pubStatus(request):
+    """Renders the createEvent page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/pubStatus.html',
+        {
+            'title':'Publication Status',
+            'message':'Your Event Creation page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def admin(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/admin.html',
+        {
+            'title':'Administration',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
