a6f283d7a23a33c65197071f924f65c790729170
Matt Telker <mtelker@stevens.edu>
2017-02-23 11:47:47 -0500

    added docstrings

--- webapp/models.py
+++ webapp/models.py
@@ -1,9 +1,11 @@
+"""Definition of models"""
 from django.db import models
 
-# This table will store the skeleton information about an event. To allow for
-# repeatable events, this is separated from the table with the data about
-# an individual trip.
+
 class EventInfo(models.Model):
+    """ This table will store the skeleton information about an event. To allow for
+    repeatable events, this is separated from the table with the data about
+     an individual trip."""
     EventID = models.AutoField(primary_key=True)
     EventName = models.CharField(max_length=50)
     EventDescription = models.TextField()
@@ -15,8 +17,9 @@ class EventInfo(models.Model):
     def __unicode__(self):
         return self.EventName
 
-# This table keeps track of where an event has been posted.
+
 class Postings(models.Model):
+    """This table keeps track of where an event has been posted"""
     PostingID = models.AutoField(primary_key=True)
     Facebook = models.BooleanField()
     MeetUp = models.BooleanField()
