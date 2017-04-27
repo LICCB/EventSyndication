f8557ed563385933319d06883a2c39a088e98b27
matttelker <matttelker@gmail.com>
2017-04-27 00:33:28 -0400

    added ability to delete events with no pubs

--- webapp/views.py
+++ webapp/views.py
@@ -99,7 +99,10 @@ def pubStatus(request):
     """Renders the createEvent page."""
     assert isinstance(request, HttpRequest)
     events = EventInfo.objects.all().order_by('-EventStart')
-    if request.method == "POST":
+    if request.method == "POST" and request.POST.get('deleteEvent') == "true":
+        EventInfo.objects.filter(id=request.POST.get('EventID')).delete()
+        notice = 'Event deleted successfully.'
+    elif request.method == "POST":
         event = events.get(id=request.POST.get('EventID'))
         postings = Publications.objects.filter(EventID=event)
         return render(
@@ -111,10 +114,11 @@ def pubStatus(request):
                 'year':datetime.now().year,
                 'event': event,
                 'events': events,
-                'publications': postings
+                'publications': postings,
             }
         )
-
+    else:
+        notice = None
     event = events.first()
     postings = Publications.objects.filter(EventID=event)
     return render(
@@ -126,7 +130,8 @@ def pubStatus(request):
             'year':datetime.now().year,
             'events': events,
             'event': event,
-            'publications': postings
+            'publications': postings,
+            'notice': notice
         }
     )
 
