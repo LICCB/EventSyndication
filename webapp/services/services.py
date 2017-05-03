b9bc3b6e54d944a108be70fbe81ec10003c8c979
matttelker <matttelker@gmail.com>
2017-05-03 02:10:40 -0400

    eventbrite posting working

--- webapp/services/services.py
+++ webapp/services/services.py
@@ -1,8 +1,10 @@
 from webapp.services.google_calendar import GoogleCalendar
 from webapp.services.facebook import Facebook
 from webapp.services.mailchimp import MailChimp
+from webapp.services.eventbrite import Eventbrite
 services = {
     'Google Calendar': GoogleCalendar(),
     'Facebook': Facebook(),
-    'Mailchimp': MailChimp()
+    'Mailchimp': MailChimp(),
+    'Eventbrite': Eventbrite()
 }
